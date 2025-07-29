4921eaf6f09df58f38036dd7a458ce4d0b4677be
ki9-feb11 <kara.kiku0211@gmial.com>
2025-07-29 00:17:56 +0000

    [Add]public/products

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,10 @@
 class Public::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all.order(:name, :manufacturer, :category)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
